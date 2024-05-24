d500a7959ac7b19d514a6b0aacf7e7fae59b54a3
Henrique De Franca Martins <henrique.martins@ciahering.com.br>
2024-05-24 17:50:18 -0300

    Chore: Started firt migration

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,8 +1,11 @@
 import { knex as setUpKnex } from "knex";
 
-export const knex = setUpKnex({
+export const config = {
   client: "sqlite",
   connection: {
     filename: "./temp/app.db"
   },
-});
+  useNullAsDefault: true
+}
+
+export const knex = setUpKnex(config);
