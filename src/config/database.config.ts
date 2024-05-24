3b1b8570b7ec4121632de559eee24eb4cd4d812c
Henrique De Franca Martins <henrique.martins@ciahering.com.br>
2024-05-24 17:14:06 -0300

    chore add knex and sqlite

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,6 +3,6 @@ import { knex as setUpKnex } from "knex";
 export const knex = setUpKnex({
   client: "sqlite",
   connection: {
-    filename: "./temp/app.db",
+    filename: "./temp/app.db"
   },
 });
