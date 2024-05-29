04aadaea94934811ecfb485a48e3251a6aedbb19
Henrique De Franca Martins <henrique.martins@ciahering.com.br>
2024-05-29 14:29:01 -0300

    Chore: changes to build and deploy on Render

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,16 +1,19 @@
-import { knex as setUpKnex, Knex } from "knex";
-import {env} from "../env/index"
+import { knex as setUpKnex, Knex } from 'knex'
+import { env } from '../env/index'
 
 export const config: Knex.Config = {
-  client: "sqlite",
-  connection: {
-    filename: env.DATABASE_URL
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
-    directory: './db/migrations'
-  }
+    directory: './db/migrations',
+  },
 }
 
-export const knex = setUpKnex(config);
+export const knex = setUpKnex(config)
